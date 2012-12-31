9494c92d45f1c9ccf85d5c7e2a1615639113f930
Sayanee <sayanee@gmail.com>
2012-12-31 21:32:52 +0800

    client side sorting algo done

--- public/js/main.js
+++ public/js/main.js
@@ -1,54 +1,105 @@
 require(
-[
+  [
   'jQuery',
   'Fittext',
   'Moment'
-],
-function($, Fittext, Moment) {
+  ],
+  function($, Fittext, Moment) {
 
   // Responsive Heading
   $("h1").fitText(0.55).css("opacity", 1);
 
-  // Open Source list
-  function insertRepo(num, url, name, updated, description) {
-    $("ul.open-list.second li:nth-child(" + num + ")").html(
-      '<a href="' + url + '"><p>' + name + '<span>updated ' + moment(updated).endOf('hour').fromNow() +
-      '</span></p><p class="tagline" >' + description + '</p></a>');
-  }
+  // Github API call for Repos
+  var Repo = {
+    totalList: [],
+    fetch: function(config) {
+      var len = config.length,
+        self = this,
+        count = 0;
+      config.forEach( function(configEl) {
+        var url = 'https://api.github.com/repos/' + configEl + '?callback=?';
+        $.getJSON( url, function( json ) {
+          var repo = {
+            html_url: json.data.html_url,
+            name: json.data.name,
+            pushed_at: json.data.pushed_at,
+            description: json.data.description
+          };
+          self.totalList.push(repo);
+          count++;
+          if (count === len) {
+            self.sortByDate();
+            self.insertRepo();
+          }
+        });
+      });
+    },
+    sortHelper: function(field, reverse, primer){
+      var key = function (x) {
+        return primer ? primer(x[field]) : x[field];
+      };
+      return function (a,b) {
+        var A = key(a), B = key(b);
+        if ( A < B ) {
+          return -1 * [-1,1][+!!reverse];
+        } else if ( A > B ) {
+          return +1 * [-1,1][+!!reverse];
+        } else {
+          return 0 * [-1,1][+!!reverse];
+        }
+      };
+    },
+    sortByDate: function () {
+      // console.log(this.totalList);
+      this.totalList.sort(
+        this.sortHelper('pushed_at', false, function(a) {
+          return new Date(a);
+        })
+      );
+      // console.log('inside sortByDate');
+      // console.log(this.totalList);
+    },
+    insertRepo: function (  ) {
+      var arr = this.totalList;
 
-  $.getJSON("https://api.github.com/repos/miccheng/php-queue?callback=?", function(json) {
-    insertRepo(1, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
+      for( var arrayIndex = 0; arrayIndex < arr.length; arrayIndex++ ) {
+        // console.log(arr[arrayIndex]);
+        $("ul.open-list.second li:nth-child(" + (arrayIndex + 1) + ")").html(
+          '<a href="'+
+          arr[arrayIndex].html_url +
+          '"><p>' +
+          arr[arrayIndex].name +
+          '<span>updated ' +
+          moment(arr[arrayIndex].pushed_at).endOf('hour').fromNow() +
+          '</span></p><p class="tagline" >' +
+          arr[arrayIndex].description +
+          '</p></a>'
+        );
+      }
+    }
+  };
+
+  // Non Github Repo
+  Repo.totalList.push({
+    html_url: "http://www.sketchkit.com/",
+    name: "Sketch Kit Wireframes",
+    pushed_at: "2012-10-01",
+    description: "Create quick iPhone app wireframes in Keynote"
   });
-  $.getJSON("https://api.github.com/repos/MugunthKumar/MKNetworkKit?callback=?", function(json) {
-    insertRepo(2, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
-  $.getJSON("https://api.github.com/repos/Luracast/Restler?callback=?", function(json) {
-    insertRepo(3, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
-  $.getJSON("https://api.github.com/repos/lxcid/LXReorderableCollectionViewFlowLayout?callback=?", function(json) {
-    insertRepo(4, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
-  $.getJSON("https://api.github.com/repos/honcheng/PaperFold-for-iOS?callback=?", function(json) {
-    insertRepo(5, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
-  $.getJSON("https://api.github.com/repos/cheeaun/hackerweb?callback=?", function(json) {
-    insertRepo(6, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
-  $.getJSON("https://api.github.com/repos/MugunthKumar/MKStoreKit?callback=?", function(json) {
-    insertRepo(7, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
-  $.getJSON("https://api.github.com/repos/zz85/sparks.js?callback=?", function(json) {
-    insertRepo(8, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
-  $.getJSON("https://api.github.com/repos/uzyn/opauth?callback=?", function(json) {
-    insertRepo(9, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
-  $.getJSON("https://api.github.com/repos/hboon/GlassButtons?callback=?", function(json) {
-    insertRepo(10, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
-  insertRepo(11, "http://www.sketchkit.com/", "Sketch Kit Wireframes", "2012-09-30", "Create quick iPhone app wireframes in Keynote");
-  $.getJSON("https://api.github.com/repos/petejkim/specta?callback=?", function(json) {
-    insertRepo(12, json.data.html_url, json.data.name, json.data.pushed_at, json.data.description);
-  });
+
+  // Github Repo
+  Repo.fetch([
+    'miccheng/php-queue',
+    'MugunthKumar/MKNetworkKit',
+    'lxcid/LXReorderableCollectionViewFlowLayout',
+    'Luracast/Restler',
+    'honcheng/PaperFold-for-iOS',
+    'cheeaun/hackerweb',
+    'MugunthKumar/MKStoreKit',
+    'zz85/sparks.js',
+    'uzyn/opauth',
+    'hboon/GlassButtons',
+    'petejkim/specta'
+    ]);
 
 });
