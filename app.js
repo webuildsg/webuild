f98ef76075b10ddfe40c636576d240be1745a70b
Sayanee <sayanee@gmail.com>
2013-02-22 12:11:41 +0800

    removed console log

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.get("/", function(req, res) {
   res.render("index.jade", { name: 'Sayanee'});
 });
 
-console.log("Express server listening on port " + app.get('port'));
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
