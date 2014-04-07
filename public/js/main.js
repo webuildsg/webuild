3e792e2d1f9b88336f21d9f311e403eedd12f63e
Sayanee <sayanee@gmail.com>
2014-04-07 20:01:04 +0800

    events and next podcast

--- public/js/main.js
+++ public/js/main.js
@@ -89,7 +89,7 @@ require(
   setInterval(countdown, 1000);
   function countdown () {
     var now = moment(),
-    podcastDate = "2014-04-05 11:00 +0800",
+    podcastDate = "2014-04-19 11:00 +0800",
     dateFormat = "YYYY-MM-DD HH:mm Z",
     livedate = moment(podcastDate, dateFormat),
     then = moment(podcastDate, dateFormat);
