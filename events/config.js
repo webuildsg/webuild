1c19be3f61366ade29a59b9a480beb1000e24bce
Alyssa <alyssaquek@gmail.com>
2014-07-12 22:14:04 +0800

    Added comment stating that 34 = meetup tech category

--- events/config.js
+++ events/config.js
@@ -2,7 +2,7 @@ module.exports = {
   meetupParams: {
     key: process.env.MEETUP_API_KEY,
     country: 'SG',
-    category: 34,
+    category: 34, // Tech category
     page: 500,
     next_event: true
   },
