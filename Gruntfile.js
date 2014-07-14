a0593e638019d44142971ee9d6305ecbdcb77326
Sayanee <sayanee@gmail.com>
2014-07-14 23:09:57 +0800

    grunt task now build sass to css

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,20 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    clean: ['public/css/style.css']
+    clean: ['public/css/style.css'],
+    shell: {
+      sass: {
+        options: {
+          stderr: true
+        },
+        command: 'sass --compass public/css/style.sass:public/css/style.css --style compressed'
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.registerTask('default', 'clean');
-  grunt.registerTask('travis', 'clean');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('default', ['clean', 'shell']);
 
 };
