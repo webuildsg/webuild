0ad2d906d08cb6fdce25074d8fa4880c101fede8
Sayanee <sayanee@gmail.com>
2014-07-14 23:43:48 +0800

    minified css and js with grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,9 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     clean: ['public/css/style.css'],
+
     shell: {
       sass: {
         options: {
@@ -10,12 +12,29 @@ module.exports = function(grunt) {
         },
         command: 'sass --compass public/css/style.sass:public/css/style.css --style compressed'
       }
+    },
+
+    uglify: {
+      production: {
+        options: {
+          mangle: false,
+          compress: true,
+          beautify: false
+        },
+        files: {
+          'public/js/script.js': [
+            'public/js/vendor/moment/min/moment.min.js',
+            'public/js/main.js'
+          ]
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['clean', 'shell']);
+  grunt.registerTask('default', ['clean', 'shell', 'uglify']);
 
 };
