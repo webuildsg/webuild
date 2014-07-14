9694b0c2ccdede76f8a4bf7108ce621ea35a9b6e
Alyssa <alyssaquek@gmail.com>
2014-07-15 03:21:41 +0800

    passportjs need default strings

--- events/setup-passport.js
+++ events/setup-passport.js
@@ -4,8 +4,8 @@ var config = require('./config');
 
 var strategy = new Auth0Strategy({
   domain: config.auth0.domain,
-  clientID: config.auth0.clientId,
-  clientSecret: config.auth0.clientSecret,
+  clientID: config.auth0.clientId || '-',
+  clientSecret: config.auth0.clientSecret || '-',
   callbackURL:  '/callback'
 }, function(accessToken, refreshToken, profile, done) {
   console.log('Received Facebook profile from: ', profile.displayName);
