2dc969f2b75b09dc90b315e0edcb699208adc28b
Chinmay Pendharkar <notthetup@gmail.com>
2015-08-15 16:48:44 +0800

    changing chron task external url

--- .openshift/cron/daily/archives.sh
+++ .openshift/cron/daily/archives.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-curl -X POST --data "secret=$WEBUILD_API_SECRET" $OPENSHIFT_IOJS_IP:$OPENSHIFT_IOJS_PORT/api/v1/archives/update
+curl -X POST --data "secret=$WEBUILD_API_SECRET" $WEBUILD_URL/api/v1/archives/update
