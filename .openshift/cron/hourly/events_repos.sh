635f3abd6744347ffbafeb655ea833a7b2ae66ff
Sayanee <sayanee@gmail.com>
2015-09-27 17:53:42 +0800

    fix: add dollar sign for archives snapshot

--- .openshift/cron/hourly/events_repos.sh
+++ .openshift/cron/hourly/events_repos.sh
@@ -3,7 +3,7 @@ curl -X POST --data "secret=$WEBUILD_API_SECRET" $WEBUILD_URL/api/v1/events/upda
 curl -X POST --data "secret=$WEBUILD_API_SECRET" $WEBUILD_URL/api/v1/repos/update
 
 # Run Archive at 3am
-hour=(date '+%H')
+hour=$(date '+%H')
 if [ $hour == 03 ]; then
 	curl -X POST --data "secret=$WEBUILD_API_SECRET" $WEBUILD_URL/api/v1/archives/update
 fi
