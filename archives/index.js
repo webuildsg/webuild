44a7aa31e3ca7f00aab52c80583aaef0b06d418e
Sayanee <sayanee@gmail.com>
2017-12-05 08:12:44 +0800

    Change branch from gh-pages to master

--- archives/index.js
+++ archives/index.js
@@ -8,7 +8,7 @@ module.exports = {
   'init': function (config) {
     function getBranchName () {
       if (process.env.NODE_ENV === 'production') {
-        return 'gh-pages'
+        return 'master'
       }
 
       return 'staging'
